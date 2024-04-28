118d7910012c44ff885c69ba08d41b09e23c16b9
kimdaye77 <kimdaye77@naver.com>
2024-04-28 18:05:51 +0900

    [Feat] 로그인 모달 공통으로 파일 분

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import { ReactNode } from "react";
 import styles from "@/app/page.module.css";
 type Props = { children: ReactNode; modal: ReactNode };
 export default function Layout({ children, modal }: Props) {
