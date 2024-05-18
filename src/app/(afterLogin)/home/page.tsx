b8f630f2b456f915637804c0c91d5890480739dc
kimdaye77 <kimdaye77@naver.com>
2024-05-18 17:18:00 +0900

    [Feat] post 구현

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -2,6 +2,7 @@ import Tab from "@/app/(afterLogin)/home/_components/Tab";
 import style from "./home.module.css";
 import TabProvider from "@/app/(afterLogin)/home/_components/TabProvider";
 import PostForm from "@/app/(afterLogin)/home/_components/PostForm";
+import Post from "@/app/(afterLogin)/_component/Post";
 
 export default function Home() {
   return (
@@ -9,9 +10,9 @@ export default function Home() {
       <TabProvider>
         <Tab />
         <PostForm />
-        {/* <Post />
         <Post />
-        <Post /> */}
+        <Post />
+        <Post />
       </TabProvider>
     </main>
   );
