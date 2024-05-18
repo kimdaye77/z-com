3cf5e1a46f1e668fec65fbf3c8d832fb2de7b769
kimdaye77 <kimdaye77@naver.com>
2024-05-18 17:10:50 +0900

    [Feat] postform 구현

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,14 +1,15 @@
 import Tab from "@/app/(afterLogin)/home/_components/Tab";
 import style from "./home.module.css";
 import TabProvider from "@/app/(afterLogin)/home/_components/TabProvider";
+import PostForm from "@/app/(afterLogin)/home/_components/PostForm";
 
 export default function Home() {
   return (
     <main className={style.main}>
       <TabProvider>
         <Tab />
-        {/* <PostForm />
-        <Post />
+        <PostForm />
+        {/* <Post />
         <Post />
         <Post /> */}
       </TabProvider>
